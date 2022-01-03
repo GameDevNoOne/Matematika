5311cfa5918e3c9e2eaab4b4d66c3495709b9660
Jakob Vuk <zlati.volk@gmail.com>
2022-01-03 21:36:30 +0100

    Začetni Commit

---
+++ Matematika.cs
@@ -0,0 +1,18 @@
+﻿namespace Matematika
+{
+    /// <summary>Matematične funkcije</summary>
+    public static class Funkcije
+    {
+        public static double Abs(double x)
+    {
+        if(x > 0)
+        {
+            return x;
+        }
+        else
+        {
+            return x*-1;
+        }
+    }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Matematika;
+namespace Main
+{
+    public static class Uporaba
+{
+    public static void Main()
+    {
+        Console.WriteLine("Napiši številko:");
+        string? s = Console.ReadLine();
+
+        if(s == null)
+        {
+            Console.WriteLine("Niste napisali številke");
+            return;
+        }
+
+        bool result = double.TryParse(s, out double x);
+
+        if(result)
+        {
+            Console.WriteLine("Absolutna vrednost je {0}", Funkcije.Abs(x));
+            return;
+        }
+        Console.WriteLine("Ni uspelo prebrati številke");
+    }
+}
+}
